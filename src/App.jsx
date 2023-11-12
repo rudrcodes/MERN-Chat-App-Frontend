ba38354d970ac19ea22af5999c692b86d856b160
Rudransh <rudrcodes@gmail.com>
2023-11-13 00:39:24 +0530

    Added env var

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import axios from 'axios'
 import { UserContextProvider } from './UserContext';
 import Routes from './Routes';
 
+
 function App() {
   // Settint axios defaults
-  axios.defaults.baseURL = "http://localhost:4000"
+  // axios.defaults.baseURL = "http://localhost:4000"
+  axios.defaults.baseURL = import.meta.env.VITE_AXIOS_DEFAULTS_BASEURL
 
+  console.log(import.meta.env.VITE_AXIOS_DEFAULTS_BASEURL)
   // This is done so we can set cokkies from our api
   axios.defaults.withCredentials = true;
 
--- src/Chat.jsx
+++ src/Chat.jsx
@@ -11,6 +11,8 @@ import Avatar from "./Avatar";
 import { UserContext } from "./UserContext";
 import Image from "./Image";
 
+const backendHostedLink = import.meta.env.VITE_AXIOS_DEFAULTS_BASEURL;
+
 const Chat = () => {
     const { usernameContext, idContext } = useContext(UserContext)
     const [sio, setSio] = useState(null)
@@ -94,7 +96,7 @@ const Chat = () => {
 
         let cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
         // console.log(cookieValue)
-        const socket = io('http://localhost:4000', {
+        const socket = io(backendHostedLink, {
             extraHeaders: {
                 usercookie: cookieValue
             }
@@ -437,7 +439,7 @@ const Chat = () => {
                                                                     <path fillRule="evenodd" d="M19.902 4.098a3.75 3.75 0 00-5.304 0l-4.5 4.5a3.75 3.75 0 001.035 6.037.75.75 0 01-.646 1.353 5.25 5.25 0 01-1.449-8.45l4.5-4.5a5.25 5.25 0 117.424 7.424l-1.757 1.757a.75.75 0 11-1.06-1.06l1.757-1.757a3.75 3.75 0 000-5.304zm-7.389 4.267a.75.75 0 011-.353 5.25 5.25 0 011.449 8.45l-4.5 4.5a5.25 5.25 0 11-7.424-7.424l1.757-1.757a.75.75 0 111.06 1.06l-1.757 1.757a3.75 3.75 0 105.304 5.304l4.5-4.5a3.75 3.75 0 00-1.035-6.037.75.75 0 01-.354-1z" clipRule="evenodd" />
                                                                 </svg>
 
-                                                                <a className="underline" href={"http://localhost:4000/uploads/" + msg.message} target="_">{msg.message}</a>
+                                                                <a className="underline" href={backendHostedLink + "/uploads/" + msg.message} target="_">{msg.message}</a>
                                                             </div>
                                                             // <a href=`http://localhost:4000/uploads/${msg.message}`  target="_">{msg.message}</a>
                                                             // const blob=new Blob()
@@ -470,7 +472,7 @@ const Chat = () => {
                                                                         <path fillRule="evenodd" d="M19.902 4.098a3.75 3.75 0 00-5.304 0l-4.5 4.5a3.75 3.75 0 001.035 6.037.75.75 0 01-.646 1.353 5.25 5.25 0 01-1.449-8.45l4.5-4.5a5.25 5.25 0 117.424 7.424l-1.757 1.757a.75.75 0 11-1.06-1.06l1.757-1.757a3.75 3.75 0 000-5.304zm-7.389 4.267a.75.75 0 011-.353 5.25 5.25 0 011.449 8.45l-4.5 4.5a5.25 5.25 0 11-7.424-7.424l1.757-1.757a.75.75 0 111.06 1.06l-1.757 1.757a3.75 3.75 0 105.304 5.304l4.5-4.5a3.75 3.75 0 00-1.035-6.037.75.75 0 01-.354-1z" clipRule="evenodd" />
                                                                     </svg>
 
-                                                                    <a className="underline" href={"http://localhost:4000/uploads/" + msg.message} target="_">{msg.message}</a>
+                                                                    <a className="underline" href={backendHostedLink + "/uploads/" + msg.message} target="_">{msg.message}</a>
                                                                 </div>                                                     // const blob=new Blob()
                                                                 : <span className="text-bold ">  {msg.message}</span>
                                                             }
